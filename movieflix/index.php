34a774ca493e0f407202c932997318a768c704a6
Emanuel Onofre <emanuelonofre@csus.edu>
2023-01-16 20:57:26 -0800

    Connecting to our database

--- movieflix/index.php
+++ movieflix/index.php
@@ -21,14 +21,39 @@
                 <button id="edit-button">Edit Record</button>
                 <button id="delete-button">Delete Record</button>
 
-                <form action="" method="POST" id="create-form">
-                    <input type="text" placeholder="Enter movie title" name=""/><br />
-                    <input type="text" placeholder="Enter movie genre" name=""/><br />
-                    <input type="text" placeholder="Enter movie director" name=""/><br />
-                    <input type="submit" value="Save"/>
+                <form action="index.php" method="POST" id="create-form">
+                    <input type="text" placeholder="Enter movie title" name="create-title"/><br />
+                    <input type="text" placeholder="Enter movie genre" name="create-genre"/><br />
+                    <input type="text" placeholder="Enter movie director" name="create-director"/><br />
+                    <input type="submit" value="Save" name="create-button"/>
 
                 </form>
-                
+                <?php
+                    //Create record function
+
+                    function createRecord(){
+
+                        $servername = 'localhost';
+                        $username = 'root';
+                        $password = '';
+                        $database = 'movieflix_database';
+
+                        //Creating a Connection to database
+
+                        $connection = mysqli_connect($servername,$username,$password,$database);
+
+                        //Check if connection wa successful or not
+                        if(!$connection){
+                            die('Connection unsuccessful : '.mysqli_connect_error());
+                        } else{
+                            echo 'Connection success!';
+                        }
+                    }
+                    if(isset($_POST['create-button'])){
+
+                        createRecord();
+                    }
+                ?>
             </div>
                 <script src="script.js"></script>
         </body>
