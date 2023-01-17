b5fb83c253512e6fffa693154648c8956dddc261
Emanuel Onofre <emanuelonofre@csus.edu>
2023-01-16 19:14:09 -0800

    Add files via upload
    Getting Started

---
+++ movieflix/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <title>MovieFlix CRUD App</title>
+        </head>
+        <body>
+            <div>
+                <h2>Movieflix CRUD Application</h2>
+            </div>
+
+        </body>
