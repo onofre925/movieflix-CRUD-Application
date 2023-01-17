66139a931f49d76ffc876dd1477d082cfd7e7b1d
Emanuel Onofre <emanuelonofre@csus.edu>
2023-01-16 19:34:45 -0800

    Adding button and the create form

--- movieflix/index.php
+++ movieflix/index.php
@@ -5,7 +5,20 @@
         </head>
         <body>
             <div>
-                <h2>Movieflix CRUD Application</h2>
+                <h2>Movieflix CRUD</h2>
+            </div>
+            <div class="content-wrapper">
+                <button id="create-button">Create Record</button>
+                <button id="edit-button">Edit Record</button>
+                <button id="delete-button">Delete Record</button>
+
+                <form action="" method="POST" id="create-form">
+                    <input type="text" placeholders="Enter movie title" name=""/>
+                    <input type="text" placeholders="Enter movie genre" name=""/>
+                    <input type="text" placeholders="Enter movie director" name=""/>
+
+                </form>
+                
             </div>
 
         </body>
