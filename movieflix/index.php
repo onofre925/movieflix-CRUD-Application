305359cebf59423219ff82bddca5496ec95620af
Emanuel Onofre <emanuelonofre@csus.edu>
2023-01-17 19:24:34 -0800

    Displaying the data

--- movieflix/index.php
+++ movieflix/index.php
@@ -16,6 +16,56 @@
             <?php require_once 'create.php';  ?>
             <div>
                 <h2>Movieflix CRUD</h2>
+                <?php
+                     $servername = 'localhost';
+                     $username = 'root';
+                     $password = '';
+                     $database = 'movieflix_database';
+
+                     //Creating a Connection to database
+
+                     $connection = mysqli_connect($servername,$username,$password,$database);
+
+                     //Check if connection wa successful or not
+                        if(!$connection){
+                        die('Connection unsuccessful : '.mysqli_connect_error());
+                        } else{
+                            echo 'Connection success!';
+                        }
+
+                        //Query the table for the records
+                        $sql = "SELECT * FROM movieflix_table";//set up our query
+                        $result = mysqli_query($connection, $sql);//store the result of our query into the $result
+                        $rowCount = mysqli_num_rows($result); //Method returns to us the number of rows -> $rowCount
+
+                        if($rowCount > 0){
+
+                            echo "
+                                <table>
+                                    <thread>
+                                        <tr>
+                                            <th>Record ID</th>
+                                            <th>Title</th>
+                                            <th>Genre</th>
+                                            <th>Director</th>
+                                        </tr>
+                                    </thread>
+                                
+                             ";
+                        }
+
+                ?> <!--End PHP code block -->
+                <?php
+                    while($row = $result->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo $row['id']?></td>
+                        <td><?php echo $row['title']?></td>
+                        <td><?php echo $row['genre']?></td>
+                        <td><?php echo $row['director']?></td>
+                    </tr>
+
+                <?php endwhile ?>
+                    </table>
             </div>
             <div class="content-wrapper">
                 <button id="create-button">Create Record</button>
